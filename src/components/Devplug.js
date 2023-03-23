0652dc8eae505fd19c9f2904896c7eb4fdb17bce
DannyKinaro <kinarodanny15@gmail.com>
2023-03-23 14:58:26 +0300

    devplug added

--- src/components/Devplug.js
+++ src/components/Devplug.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 export default function Devplug() {
   return (
     <>
       <div className="flex justify-center my-20 cursor-pointer text-slate-600 hover:text-slate-900">
-        <a href="https://wahinya-luxuryhotel.netlify.app">More by the developer -> </a>
+        <a href="https://wahinya-luxuryhotel.netlify.app">
+          More by the developer{" "}
+        </a>
       </div>
     </>
   );

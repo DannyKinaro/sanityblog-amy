cc75ad82dcde9268dd5cd7c9245f166e59ad289d
DannyKinaro <kinarodanny15@gmail.com>
2023-03-23 16:38:09 +0300

    text slate on hover fix

--- src/components/Devplug.js
+++ src/components/Devplug.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Devplug() {
   return (
     <>
-      <div className="flex justify-center my-20 cursor-pointer text-slate-600 hover:text-slate-100">
+      <div className="flex justify-center my-20 cursor-pointer text-slate-600 hover:dark:text-slate-100 hover:text-slate-800">
         <a href="https://wahinya-luxuryhotel.netlify.app">
           More by the developer{" "}
         </a>

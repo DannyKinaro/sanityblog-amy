152da2dc0688b92344ecc6208210203383e6c940
DannyKinaro <85222152+DannyKinaro@users.noreply.github.com>
2023-03-23 11:43:20 +0000

    devplug component

--- src/Devplug.js
+++ src/Devplug.js
@@ -4,7 +4,7 @@ export default function Devplug() {
   return (
     <>
       <div className="flex justify-center my-20 cursor-pointer text-slate-600 hover:text-slate-900">
-        <a href="https://wahinya-luxuryhotel.netlify.app">More by the developer -> </a>
+        <a href="https://wahinya-luxuryhotel.netlify.app">More by the developer </a>
       </div>
     </>
   );
